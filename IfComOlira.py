e4465ede028743124a1f82e98adc45272ca70588
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-09-27 21:59:28 -0300

    Update IfComOlira.py

--- IfComOlira.py
+++ IfComOlira.py
@@ -3,5 +3,12 @@ taxa = 0
 rendimento = 0.10
 
 if rendimento > meta:
-    taxa = 0.02
-    print('A taxa foi de {}'.format(taxa))
+    if rendimento > 0.20:
+        taxa = 0.04
+        print('A taxa foi de {}'.format(taxa))
+    else:
+        taxa = 0.02
+        print('A taxa foi de {}'.format(taxa))
+else:
+    taxa = 0
+    print('A taxa foi de {}.'.format(taxa))
